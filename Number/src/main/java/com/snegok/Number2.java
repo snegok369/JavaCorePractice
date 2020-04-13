e6532a8bd19b7623f8c4d77e25ff2ca1180df9b8
Sneg <snegok369>
2020-04-13 03:42:28 +0300

    Number-2 -создал два числа -применил if/else -вывел строку на экран

---
+++ Number/src/main/java/com/snegok/Number2.java
@@ -0,0 +1,20 @@
+package com.snegok;
+
+
+public class Number2 {
+    public static void main(String[] args) {
+        int num1 = 77;
+        int num2 = 77;
+        if(num1>num2)
+        {
+            System.out.println("больше");
+        }
+        else if(num1<num2)
+        {
+            System.out.println("меньше");
+        }
+        else {
+            System.out.println("равны");
+        }
+    }
+}
